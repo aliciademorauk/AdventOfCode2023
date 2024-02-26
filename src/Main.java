37f2bd00d5af83600671f1b135080b10de20c94e
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-02-26 15:37:27 +0000

    Add Day 1 AoC part 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,42 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Part 1
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(new File("PuzzleInputDay1.txt"));
+            int totalSumPartOne = 0;
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String numbersOnly = line.replaceAll("[^0-9]", "");
+
+                if (numbersOnly.length() > 1) {
+                    String stringNumber = "" + numbersOnly.charAt(0) + numbersOnly.charAt(numbersOnly.length() - 1);
+                    totalSumPartOne += Integer.parseInt(stringNumber);
+                } else {
+                    String stringNumber = "" + numbersOnly.charAt(0) + numbersOnly.charAt(0);
+                    totalSumPartOne += Integer.parseInt(stringNumber);
+                }
+            }
+
+            System.out.printf("The total sum of calibration values in Part 1 is %d", totalSumPartOne);
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
         }
     }
 }
