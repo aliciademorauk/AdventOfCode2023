b447bc2495acb440e421110cae2caada2800102c
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-03-19 19:21:31 +0000

    Refactor Day 3 code files 1 & 2 into single file extending Day abstract class

--- src/App.java
+++ src/App.java
@@ -3,6 +3,6 @@ import solutions.*;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(new Day3().solvePart1());
-        //System.out.println(new Day2().solvePart2());
+        System.out.println(new Day3().solvePart2());
     }
 }
--- src/solutions/Day3.java
+++ src/solutions/Day3.java
@@ -1,7 +1,9 @@
 package solutions;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class Day3 extends Day {
     public Day3() throws IOException {
@@ -20,14 +22,15 @@ public class Day3 extends Day {
         int indexAfterNumber;
         int startFromPosition;
         int output = 0;
+        Iterator<String> iterator = input.iterator();
 
-        for (int i = 0; i < input.size() - 1; i++) {
+        while (iterator.hasNext()) {
             if (!nextLine.isBlank()) {
                 currentLine = nextLine;
-            } else {currentLine = input.get(i + 1);}
+            } else {currentLine = iterator.next();}
 
-            if (i < input.size() - 1) {
-                nextLine = input.get(i + 1);
+            if (iterator.hasNext()) {
+                nextLine = iterator.next();
             }
 
             linesArray[0] = previousLine;
@@ -52,12 +55,8 @@ public class Day3 extends Day {
             }
             previousLine = currentLine;
         }
-
         //Processing of last line of file input
-        numbersArray = Arrays.stream(nextLine.split("\\D+"))
-                .filter(s -> !s.isBlank())
-                .toArray(String[]::new);
-
+        numbersArray = extractNumbers(nextLine);
         startFromPosition = 0;
         for (String number : numbersArray) {
             indexOfNumber = nextLine.indexOf(number, startFromPosition);
@@ -68,13 +67,72 @@ public class Day3 extends Day {
                 output += Integer.parseInt(number);
             }
         }
-
         return output;
     }
 
     @Override
     public int solvePart2() {
-        return 0;
+        String[] linesArray = new String[] {"", "", ""}; // Array with previous [0], current [1] and next [2] line
+        String[][] numbersArray = new String[3][]; // Two-dimensional array to store previous [0], current [1] and next [2] numbers' arrays
+        ArrayList<Integer> asteriskIndexes;
+        int[] rangeToCheck;
+        int[] numbersToMultiply = new int[]{0,0};
+        int output = 0;
+        Iterator<String> iterator = input.iterator();
+
+        while (iterator.hasNext()) {
+            if (!linesArray[2].isBlank()) {
+                linesArray[1] = linesArray[2];
+            } else {
+                linesArray[1] = iterator.next();
+            }
+
+            if (iterator.hasNext()) {
+                linesArray[2] = iterator.next();
+            }
+
+            for (int i = 0; i < linesArray.length; i++) {
+                numbersArray[i] = extractNumbers(linesArray[i]);
+            }
+
+            asteriskIndexes = extractAsteriskIndexes(linesArray[1]);
+            for (int asteriskIndex : asteriskIndexes) {
+                rangeToCheck = getIndexRange(linesArray[1], asteriskIndex);
+                // Check previous and next line for digits in the relevant range around the asterisk in current line
+                int y = 0;
+                int i = rangeToCheck[0];
+                while (i <= rangeToCheck[1]) {
+                    if (Character.isDigit(linesArray[0].charAt(i))) {
+                        int number = getNumberInRange(numbersArray[0], linesArray[0], i);
+                        if (!contains(numbersToMultiply, number)) {
+                            numbersToMultiply[y++] = number;
+                        }
+                    }
+
+                    if (Character.isDigit(linesArray[2].charAt(i))) {
+                        int number = getNumberInRange(numbersArray[2], linesArray[2], i);
+                        if (!contains(numbersToMultiply, number)) {
+                            numbersToMultiply[y++] = number;
+                        }
+                    }
+
+                    i++;
+                }
+                // Check current line
+                for (int number : rangeToCheck) {
+                    if (Character.isDigit(linesArray[1].charAt(number))) {
+                        numbersToMultiply[y] = getNumberInRange(numbersArray[1], linesArray[1], number);
+                        y++;
+                    }
+                }
+                if (Arrays.stream(numbersToMultiply).allMatch(num -> num != 0)) {
+                    output += numbersToMultiply[0] * numbersToMultiply[1];
+                }
+                Arrays.fill(numbersToMultiply, 0);
+            }
+            linesArray[0] = linesArray[1];
+        }
+        return output;
     }
 
     private String[] extractNumbers(String line) {
@@ -86,12 +144,62 @@ public class Day3 extends Day {
     private int setBeforeIndex(int indexOfNumber) {
         return indexOfNumber == 0 ? indexOfNumber : indexOfNumber - 1;
     }
+
     private int setAfterIndex(String line, String number, int indexOfNumber) {
         return (indexOfNumber + number.length() < line.length()) ? indexOfNumber + number.length() : indexOfNumber + number.length() - 1;
     }
+
     private boolean hasSymbolInRange (String line, int indexBeforeNumber, int indexAfterNumber) {
         for (int i = indexBeforeNumber; i <= indexAfterNumber; i++) {
             if ((line.charAt(i) != '.' && !Character.isDigit(line.charAt(i)))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private ArrayList<Integer> extractAsteriskIndexes(String line) {
+        ArrayList<Integer> asteriskIndexes = new ArrayList<>();
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == '*') {
+                asteriskIndexes.add(i);
+            }
+        }
+        return asteriskIndexes;
+    }
+
+    private int[] getIndexRange(String line, int iOfSymbol) {
+        int[] rangeArray = new int[]{iOfSymbol - 1, iOfSymbol + 1};
+        if (iOfSymbol == 0) {
+            rangeArray[0] = iOfSymbol;
+        } else if (iOfSymbol == line.length() - 1) {
+            rangeArray[1] = iOfSymbol;
+        }
+        return rangeArray;
+    }
+
+    private int getNumberInRange(String[] numbers, String line, int index) {
+        int startFromPosition = 0;
+        for (String number : numbers) {
+            int index1stDigit = line.indexOf(number, startFromPosition);
+            int indexLastDigit = index1stDigit;
+            if (Integer.parseInt(number) > 9) {
+                indexLastDigit += 1;
+                if (Integer.parseInt(number) > 99) {
+                    indexLastDigit += 1;
+                }
+            }
+            if (index >= index1stDigit && index <= indexLastDigit) {
+                return Integer.parseInt(number);
+            }
+            startFromPosition = indexLastDigit;
+        }
+        return 1;
+    }
+
+    private boolean contains(int[] array, int otherNumber) {
+        for (int number : array) {
+            if (number == otherNumber) {
                 return true;
             }
         }
